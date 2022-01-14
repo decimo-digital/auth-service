44a326d8f4486462f0b5f48f6ae9da775d5abbfd
Simone <64575218+smsimone@users.noreply.github.com>
2022-01-14 19:20:09 +0100

    aggiunti log

--- src/main/java/it/decimo/auth_service/controller/gateway/PrenotationController.java
+++ src/main/java/it/decimo/auth_service/controller/gateway/PrenotationController.java
@@ -1,20 +1,5 @@
 package it.decimo.auth_service.controller.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -28,6 +13,11 @@ import it.decimo.auth_service.dto.response.BasicResponse;
 import it.decimo.auth_service.services.AuthService;
 import it.decimo.auth_service.utils.annotations.NeedLogin;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/prenotation")
@@ -42,7 +32,7 @@ public class PrenotationController {
 
     @PostMapping()
     public ResponseEntity<Object> createPrenotation(@RequestHeader("access-token") String jwt,
-            @RequestBody PrenotationRequestDto prenotationRequest) {
+                                                    @RequestBody PrenotationRequestDto prenotationRequest) {
         log.info("Sending prenotation request: {}", prenotationRequest.toString());
         return prenotationServiceConnector.makePrenotation(prenotationRequest);
     }
@@ -50,15 +40,21 @@ public class PrenotationController {
     @GetMapping()
     @ApiResponse(responseCode = "200", description = "Lista delle prenotazioni effettuate", content = @Content(array = @ArraySchema(schema = @Schema(implementation = UserPrenotation.class), minItems = 0, uniqueItems = true)))
     public ResponseEntity<Object> getUserPrenotations(@RequestHeader("access-token") String jwt,
-            @RequestParam(name = "userId", required = false) Integer requesterId) {
+                                                      @RequestParam(name = "userId", required = false) Integer requesterId) {
         if (requesterId == null) {
             requesterId = authService.getIdFromJwt(jwt);
         }
+        
+        log.info("Retrieving prenotations of user {}", requesterId);
         final var prenotations = prenotationServiceConnector.getPrenotations(requesterId);
 
+
         if (prenotations.getStatusCode() == HttpStatus.OK) {
+            log.info("Fetched {} prenotations", prenotations.getBody().size());
             return ResponseEntity.ok(prenotations.getBody());
         }
+
+        log.warn("Failed to fetch prenotations. Status code: {}", prenotations.getStatusCode());
 
         return ResponseEntity.status(prenotations.getStatusCode()).body(prenotations.getBody());
     }
@@ -70,9 +66,13 @@ public class PrenotationController {
             @ApiResponse(responseCode = "401", description = "L'utente richiedente non ha i permessi necessari per la risorsa", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
     })
     public ResponseEntity<Object> getPrenotations(@PathVariable(name = "merchantId") int merchantId,
-            @RequestHeader("access-token") String jwt) {
+                                                  @RequestHeader("access-token") String jwt) {
         int requesterId = authService.getIdFromJwt(jwt);
+
+        log.info("User {} requesting prenotations of merchant {}", requesterId, merchantId);
+
         final var response = prenotationServiceConnector.getPrenotationsForMerchant(merchantId, requesterId);
+
         if (response.getStatusCode() == HttpStatus.OK) {
             return ResponseEntity.ok(response.getBody());
         }
@@ -85,9 +85,9 @@ public class PrenotationController {
             @ApiResponse(responseCode = "200", description = "L'utente specificato è stato aggiunto alla prenotazione", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
             @ApiResponse(responseCode = "401", description = "L'utente richiedente non è l'owner della prenotazione e non può aggiungere gente", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
             @ApiResponse(responseCode = "404", description = "Non è stata trovata nessuna prenotazione esistente", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
-            @ApiResponse(responseCode = "422", description = "L'utente era già stato registrato nella prenotazione", content = @Content(schema = @Schema(implementation = BasicResponse.class))) })
+            @ApiResponse(responseCode = "422", description = "L'utente era già stato registrato nella prenotazione", content = @Content(schema = @Schema(implementation = BasicResponse.class)))})
     public ResponseEntity<Object> addUserToPrenotation(@RequestHeader("access-token") String jwt,
-            @Param(value = "prenotationId") int prenotationId, @PathVariable int userId) {
+                                                       @Param(value = "prenotationId") int prenotationId, @PathVariable int userId) {
         try {
             final var requesterId = authService.getIdFromJwt(jwt);
             return prenotationServiceConnector.addUserToPrenotation(requesterId, prenotationId, userId);
@@ -103,7 +103,7 @@ public class PrenotationController {
             @ApiResponse(responseCode = "401", description = "L'utente non può modificare la prenotazione", content = @Content(schema = @Schema(implementation = BasicResponse.class))),
     })
     public ResponseEntity<Object> editPrenotation(@RequestHeader("access-token") String jwt,
-            @RequestBody Prenotation prenotation) {
+                                                  @RequestBody Prenotation prenotation) {
         final var requesterId = authService.getIdFromJwt(jwt);
         try {
             return prenotationServiceConnector.updatePrenotation(requesterId, prenotation);
