45d6b417b5ff0cdacc25b395de45af73052fb509
Simone Masoero <smasoero@clikapp.it>
2022-01-17 15:34:47 +0100

    modificato request factory per chiamate rest

--- src/main/java/it/decimo/auth_service/AuthServiceApplication.java
+++ src/main/java/it/decimo/auth_service/AuthServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -19,6 +20,8 @@ public class AuthServiceApplication {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        final var restTemplate = new RestTemplate();
+        restTemplate.setRequestFactory(new OkHttp3ClientHttpRequestFactory());
+        return restTemplate;
     }
 }
